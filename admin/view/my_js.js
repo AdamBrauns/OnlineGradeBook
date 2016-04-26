e9c62281bfcfd2299047f0dd679c25a650945114
motorider9911 <gjones8580@live.com>
2016-04-26 13:07:10 -0500

    Files needed for the pop up version of the dash

---
+++ admin/view/my_js.js
@@ -0,0 +1,50 @@
+// Validating Empty Field
+function check_empty() {
+		document.getElementById('form').submit();
+		alert("Form Submitted Successfully...");
+}
+//Function To Display Popup
+function AddUserdiv_show() {
+document.getElementById('addUser').style.display = "block";
+}
+//Function to Hide Popup
+function AddUserdiv_hide(){
+document.getElementById('addUser').style.display = "none";
+}
+//Function To Display Popup
+function RemoveUserdiv_show() {
+document.getElementById('removeUser').style.display = "block";
+}
+//Function to Hide Popup
+function RemoveUserdiv_hide(){
+document.getElementById('removeUser').style.display = "none";
+}
+function AddClassdiv_show() {
+document.getElementById('addClass').style.display = "block";
+}
+//Function to Hide Popup
+function AddClassdiv_hide(){
+document.getElementById('addClass').style.display = "none";
+}
+//Function To Display Popup
+function RemoveClassdiv_show() {
+document.getElementById('removeClass').style.display = "block";
+}
+//Function to Hide Popup
+function RemoveClassdiv_hide(){
+document.getElementById('removeClass').style.display = "none";
+}
+function changePassdiv_show() {
+document.getElementById('changePass').style.display = "block";
+}
+//Function to Hide Popup
+function changePassdiv_hide(){
+document.getElementById('changePass').style.display = "none";
+}
+function managediv_show() {
+document.getElementById('manage').style.display = "block";
+}
+//Function to Hide Popup
+function managediv_hide(){
+document.getElementById('manage').style.display = "none";
+}
